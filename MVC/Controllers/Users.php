8e649e5eabcbdc96bde92cc76bc02045fb5faa4c
yuraonuferko <yura_onuferko@yahoo.com>
2017-01-13 21:40:15 +0200

    Add files via upload

--- MVC/Controllers/Users.php
+++ MVC/Controllers/Users.php
@@ -17,9 +17,62 @@ class Users
      */
     public function loginAction()
     {
-        Dispatcher::getInstance()->display('users/login');
-    }
+        if (!isset($_POST['email']) && !isset($_POST['password'])) {
+          Dispatcher::getInstance()->display('users/login');
+        }
+        else {
+          //temporary connect  to database
+          $dbForConnect = include 'config/database.php';
+          $link = mysqli_connect($dbForConnect['host'], $dbForConnect['username'],
+            $dbForConnect['password'],$dbForConnect['database'])
+            or die("Could not connect: " . mysqli_error($link));
 
+          if (mysqli_connect_errno()) {
+            printf("Підключення до сервера MySQL неможливе. Код помилки: %s\n", mysqli_connect_error());
+            exit;
+          }
+          $login = self::SQLSecurity($link,$_POST['email']);
+          $password =  self::SQLSecurity($link,$_POST['password']);
+          $query =
+              "SELECT * FROM users WHERE email='$login' AND password='$password';";
+          $result = mysqli_query($link, $query);
+          $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+          if ($row) {
+            mysqli_free_result($result);
+            mysqli_close($link);
+            session_start();
+            $_SESSION['id'] = $row['id'];
+            $row['session'] = $_SESSION['id'];
+            echo "Ви успішно зараєстровані!!!".'<br>';
+            return var_dump($row);
+          }
+          else {
+            echo "Невірний логін/пароль";
+            mysqli_free_result($result);
+            mysqli_close($link);
+          }
+        }
+      }
+  /**
+   * Security for SQL injection
+   * @var $link $data
+   * @ return $data
+   */
+    public static function SQLSecurity($link,$data)
+  {
+    $text_to_check = mysqli_real_escape_string ($link,$data);
+
+    $text_to_check = strip_tags($text_to_check);
+
+    $text_to_check = htmlspecialchars($text_to_check);
+
+    $text_to_check = stripslashes($text_to_check);
+
+    $text_to_check = addslashes($text_to_check);
+
+    $data = $text_to_check;
+    return $data;
+  }
     /**
      * Register action
      */
