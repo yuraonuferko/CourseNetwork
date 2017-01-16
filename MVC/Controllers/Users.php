27ceac7af498f680f51b74409d07adf891af6854
yuraonuferko <yura_onuferko@yahoo.com>
2017-01-16 12:46:38 +0200

    Add files via upload
    implemented  and testing  - LogginAction() and RegisterAction()
    I replaced in file MVC/Views/users/register.phtml str 6 from <form action="/users/login" method="post"> 
    at <form action="/users/register" method="post">

--- MVC/Controllers/Users.php
+++ MVC/Controllers/Users.php
@@ -11,7 +11,6 @@ use System\Dispatcher;
  */
 class Users
 {
-
     /**
      * Login action
      */
@@ -31,8 +30,16 @@ class Users
           }
           $login = self::SQLSecurity($link,$_POST['email']);
           $password =  self::SQLSecurity($link,$_POST['password']);
+          $options = [
+            'salt' => md5($password),
+            //write your own code to generate a suitable salt
+            'cost' => 12
+            // the default cost is 10
+          ];
+          $hash = password_hash($password, PASSWORD_DEFAULT, $options);
+
           $query =
-              "SELECT * FROM users WHERE email='$login' AND password='$password';";
+              "SELECT * FROM users WHERE email='$login' AND password='$hash';";
           $result = mysqli_query($link, $query);
           $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
           if ($row) {
@@ -82,7 +89,60 @@ class Users
     public function registerAction()
     {
       /*register new user */
-      Dispatcher::getInstance()->display('users/register');
+
+      if (!isset($_POST['email']) && !isset($_POST['password']) &&
+        !isset($_POST['name']) ) {
+        Dispatcher::getInstance()->display('users/register');
+      }
+      else {
+        $dbForConnect
+          = Config::getInstance()->get('database');
+        $link = mysqli_connect($dbForConnect['host'], $dbForConnect['username'],
+          $dbForConnect['password'], $dbForConnect['database']);
+        if ($link == FALSE) {
+          echo "Підключення до сервера MySQL неможливе.<br> Спробуйте пізніше";
+          Dispatcher::getInstance()->display('home/index');
+        }
+        $login = self::SQLSecurity($link, $_POST['email']);
+        $password = self::SQLSecurity($link, $_POST['password']);
+        $name = self::SQLSecurity($link, $_POST['name']);
+        $options = [
+          'salt' => md5($password),
+          //write your own code to generate a suitable salt
+          'cost' => 12
+          // the default cost is 10
+        ];
+        $hash = password_hash($password, PASSWORD_DEFAULT, $options);
+        $query_login =
+          "SELECT * FROM users WHERE email='$login';";
+        $result = mysqli_query($link, $query_login);
+        $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+        if (!$row){
+          $query =
+            "INSERT INTO `users` ( `name` , `email` , `password` ) 
+           VALUES ('$name', '$login', '$hash');";
+          $result = mysqli_query($link, $query);
+          if ($result) {
+            mysqli_free_result($result);
+            mysqli_close($link);
+            echo "Вітаємо з успішною реєстрацією". $name.'<br>'
+              .'Авторизуйтесь, будь-ласка<br>';
+            Dispatcher::getInstance()->display('users/login');
+          }
+        }
+        else {
+          $message = "Користувач $login вже існує.<br> Змініть email\n";
+          if ($link == FALSE){$message = '';}
+          echo $message;
+          mysqli_free_result($result);
+          mysqli_close($link);
+          Dispatcher::getInstance()->display('users/register');
+        }
+
+
+
+
+      }
     }
 
     public function testAction()
